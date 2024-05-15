e08d3f0f982fdc9c00f3ea21ab5c32100caced06
developer-mixa <mixail_081177@mail.ru>
2024-05-15 16:18:58 +0300

    docs: model/utils/crud

--- model/utils/crud.py
+++ model/utils/crud.py
@@ -1,46 +1,86 @@
-from typing import Any
-from abc import abstractmethod
-from sqlalchemy import Engine, select, delete
-from sqlalchemy.orm import Session
-from model.utils.exceptions import NotFound
+"""Model with CrudRepository."""
+from typing import type
 from uuid import UUID
-from sqlalchemy import text, cast, UUID as sql_UUID
+
+from sqlalchemy import Engine, select, text
+from sqlalchemy.orm import Session
+
+from model.utils.exceptions import NotFound
+
 
 class CrudRepository:
+    """Base class that implements KRUD for the database.
+
+    Example:
+        class PersonRepository(CrudRepository):
+        _engine = your_engine()
+        _model_class = Person
+    """
 
     _engine: Engine
-    _model_class: Any
+    _model_class: type
 
-    def get(self) -> list[Any]:
+    def get(self) -> list[type]:
+        """Get all model class objects.
+
+        Returns:
+            list[type]: All model class objects
+        """
         with Session(self._engine) as session:
             return session.scalars(select(self._model_class)).fetchall()
-    
-    def add(self, model_class: Any) -> None:
+
+    def add(self, model_class: type) -> UUID:
+        """Add model class to database.
+
+        Args:
+            model_class (type): Model class which we need to add
+
+        Returns:
+            UUID: Added object id
+        """
         with Session(self._engine) as session:
             session.add(model_class)
             session.commit()
             return model_class.id
-    
-    def update(self, updated_model_class: Any):
+
+    def update(self, updated_model_class: type) -> UUID:
+        """Update object by Updated class.
+
+        Args:
+            updated_model_class (type): Model database class
+
+        Raises:
+            NotFound: Raise when object with this id is not found
+
+        Returns:
+            UUID: Updated object id
+        """
         with Session(self._engine) as session:
             table_name = self._model_class.__tablename__
-            query = text(f"SELECT * FROM {table_name} WHERE id = :id")
-            model_str = session.execute(query, {"id" : updated_model_class.id}).fetchone()
+            query = text(f'SELECT * FROM {table_name} WHERE id = :id')
+            model_str = session.execute(query, {'id': updated_model_class.id}).fetchone()
             model = self._model_class(**model_str._asdict())
             if not model:
                 raise NotFound(self._model_class)
-            for attr, value in vars(updated_model_class).items():
-                if hasattr(model, attr):
-                    setattr(model, attr, value)  
-            session.merge(model)         
+            for attr, attr_value in vars(updated_model_class).items():
+                setattr(model, attr, attr_value)
+            session.merge(model)
             session.commit()
             return model.id
-    
-    def remove(self, id: UUID):
+
+    def remove(self, model_id: UUID):
+        """Remove object by id.
+
+        Args:
+            model_id (UUID): Object id which we need to delete
+
+        Raises:
+            NotFound: Raise when object with this id is not found
+        """
         with Session(self._engine) as session:
             table_name = self._model_class.__tablename__
-            query = text(f"DELETE FROM {table_name} WHERE id = :id")
-            result = session.execute(query, {"id" : id})
+            query = text(f'DELETE FROM {table_name} WHERE id = :id')
+            removed_id = session.execute(query, {'id': model_id})
             session.commit()
-            if result.rowcount == 0:
+            if removed_id.rowcount == 0:
                 raise NotFound(self._model_class)
--- model/utils/db.py
+++ model/utils/db.py
@@ -6,6 +6,7 @@ from sqlalchemy import Engine
 from sqlalchemy import create_engine as create_engine_sql
 
 PG_VARS = 'PG_HOST', 'PG_PORT', 'PG_USER', 'PG_PASSWORD', 'PG_DBNAME'
+DB_URL = 'postgresql+psycopg://'
 
 
 def __get_db_url() -> str:
@@ -16,7 +17,7 @@ def __get_db_url() -> str:
     """
     dotenv.load_dotenv()
     credentials = {pg_var: os.environ.get(pg_var) for pg_var in PG_VARS}
-    return 'postgresql+psycopg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DBNAME}'.format(**credentials)
+    return DB_URL + '{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DBNAME}'.format(**credentials)
 
 
 def create_engine() -> Engine:
