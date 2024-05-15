ee0fa7968525e201c93179c6b8559eae4e78b208
developer-mixa <mixail_081177@mail.ru>
2024-05-15 16:26:09 +0300

    docs: model/utils/exceptions

--- model/utils/exceptions.py
+++ model/utils/exceptions.py
@@ -1,3 +1,14 @@
+"""All exception in model layer."""
+
+
 class NotFound(Exception):
-    def __init__(self, class_) -> None:
-        super().__init__(f'{str(class_)} was not found!')
+    """Exception when there is no model class in database."""
+
+    def __init__(self, model_class: type) -> None:
+        """Init NotFound exception.
+
+        Args:
+            model_class : Database model
+        """
+        class_name = str(model_class)
+        super().__init__(f'{class_name} was not found!')
