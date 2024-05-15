7f89492330d140dee6b2149808ad92db909f80ad
developer-mixa <mixail_081177@mail.ru>
2024-05-15 17:09:37 +0300

    docs: film_repository

--- model/film_repository.py
+++ model/film_repository.py
@@ -1,7 +1,12 @@
+"""Module for film repository."""
+
 from model.models import Film
-from model.utils.db import create_engine
 from model.utils.crud import CrudRepository
+from model.utils.db import create_engine
+
 
 class FilmRepository(CrudRepository):
+    """Repository for working with films."""
+
     _engine = create_engine()
     _model_class = Film
